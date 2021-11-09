f22b8635fa83b702f5bf2e5f4ae26b564dae62fc
Kirill Vasilkov <vasilkov.kv@perfema.com>
2021-11-09 20:10:41 +0300

    feat(styles): add global color constants

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,24 @@
 module.exports = {
-    chainWebpack: (config) => {
-        const svgRule = config.module.rule('svg');
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
 
-        svgRule.uses.clear();
+    svgRule.uses.clear();
 
-        svgRule
-            .use('babel-loader')
-            .loader('babel-loader')
-            .end()
-            .use('vue-svg-loader')
-            .loader('vue-svg-loader');
+    svgRule
+      .use("babel-loader")
+      .loader("babel-loader")
+      .end()
+      .use("vue-svg-loader")
+      .loader("vue-svg-loader");
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/assets/_colors.scss";`,
+        sassOptions: {
+          includePaths: ["@/assets"],
+        },
+      },
     },
+  },
 };
